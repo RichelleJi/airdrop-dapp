f2d3570ef64b0abc4dbb7053be95f1060fcdffcd
richelleji <ruixi.ji@gmail.com>
2023-01-23 17:27:05 -0500

    add app config and README file

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+interface networkConfig {
+    airDropAddress: string;
+    magnaTokenAddress: string;
+}
+
+const goerliEnvConfig: networkConfig = {
+  airDropAddress: "0x89A43C7e2C05DD82870D11D67Cc99628D467CFb7",
+  magnaTokenAddress: "0x9Ee30Ad1788E683414bED35cfcD563ce1062581e"
+};
+
+export default goerliEnvConfig
